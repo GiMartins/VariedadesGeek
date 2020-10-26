e804578b14920fc7724b657aa180a0cebb7570d8
Giovana <gih_1997@hotmail.com>
2020-10-26 20:11:52 -0300

    commit inicial

---
+++ src/main/java/com/senac/projetointegrador/VariedadesGeek/VariedadesGeekApplication.java
@@ -0,0 +1,13 @@
+package com.senac.projetointegrador.VariedadesGeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariedadesGeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariedadesGeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/projetointegrador/VariedadesGeek/VariedadesGeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.projetointegrador.VariedadesGeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariedadesGeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
